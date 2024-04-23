cad81f70bcba01bce6114d826820601cdf2688d8
Matheus <meduardo.valentim@gmail.com>
2024-04-23 18:34:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div className="max-w-[1216px] h-[800px] mx-auto my-4 py-5 flex flex-col gap-5 bg-gray-700 rounded-2xl">
+      <h1 className="text-3xl font-bold text-red-400 text-center bg-black">Password Generator</h1>
+      <button onClick={alert}>RODAR</button>
+    </div>
+  )
+}
